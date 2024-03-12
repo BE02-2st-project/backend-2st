53d4847b6f3d567721cf7ab965e1b7ee9718480a
SungHeeCheol <107664115+SungHeeCheol@users.noreply.github.com>
2024-03-12 17:24:32 +0900

    기본틀 추가

--- src/main/java/com/github/super_mall/repository/orderRepository/OrderRepository.java
+++ src/main/java/com/github/super_mall/repository/orderRepository/OrderRepository.java
@@ -2,6 +2,9 @@ package com.github.super_mall.repository.orderRepository;
 
 import com.github.super_mall.entity.orderEntity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+
 }
