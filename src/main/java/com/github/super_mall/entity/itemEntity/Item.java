a64f92ed171ca2ef405b6cad369c6e14f7ba29b7
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-03-13 03:28:33 +0900

    categoryRepo

--- src/main/java/com/github/super_mall/entity/itemEntity/Item.java
+++ src/main/java/com/github/super_mall/entity/itemEntity/Item.java
@@ -1,26 +1,58 @@
 package com.github.super_mall.entity.itemEntity;
 
+import com.github.super_mall.dto.itemDto.ItemAdditionalDto;
+import com.github.super_mall.entity.categoryEntity.Category;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 @Getter
 @NoArgsConstructor
-@Entity
+@AllArgsConstructor
+@Builder
+@Entity(name = "items")
 public class Item {
-    /**
-     * `item_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     * `category_id` INT NOT NULL,
-     * `item_name` VARCHAR(255) NOT NULL,
-     * `img` VARCHAR(255) NOT NULL,
-     * `price` INT NOT NULL,
-     * `stock` INT NOT NULL,
-     * `item_description` TEXT NOT NULL,
-     * `create_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-     */
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id", nullable = false)
     private Integer id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "item_name", nullable = false)
+    private String name;
+
+    @Column(name = "img")
+    private String imgURL;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
+
+    @Column(name = "item_description", nullable = false)
+    private String description;
+
+    @Column(name = "create_at", nullable = false)
+    private LocalDateTime createAt;
+
+    public static Item toEntity(ItemAdditionalDto addItem, Category category) {
+        return Item.builder()
+                .category(category)
+                .name(addItem.getName())
+                .price(addItem.getPrice())
+                .imgURL(addItem.getImgURL())
+                .description(addItem.getDescription())
+                .createAt(LocalDateTime.parse(addItem.getCreateAt(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .build();
+    }
 }
