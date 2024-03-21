754ad4d335c6aae866f8b9ae6a901733e66e237c
sang hyeok Lee <saing0813@naver.com>
2024-03-22 01:10:12 +0900

    fix: social login url

--- src/main/java/com/github/super_mall/config/oAuthConfig/OAuth2SuccessHandler.java
+++ src/main/java/com/github/super_mall/config/oAuthConfig/OAuth2SuccessHandler.java
@@ -59,8 +59,8 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         }
 
         UriComponents uriComponents = UriComponentsBuilder.newInstance()
-                        .scheme("http")
-                        .host("localhost:3000")
+                        .scheme("https")
+                        .host("frontend-supermall.vercel.app")
                         .query("accessToken={value1}")
                         .query("refreshToken={value2}")
                                 .buildAndExpand(accessToken, refreshToken);
