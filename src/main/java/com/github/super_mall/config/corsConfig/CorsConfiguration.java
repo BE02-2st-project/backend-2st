ace1419e9691f1a8f1530fe0d70bb92748a1b8cb
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-03-22 17:23:58 +0900

    .

--- src/main/java/com/github/super_mall/config/corsConfig/CorsConfiguration.java
+++ src/main/java/com/github/super_mall/config/corsConfig/CorsConfiguration.java
@@ -10,6 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings (CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
